83a21d06ed7a8c7cc5c7d1ab4a18130663adf9af
shing1223 <shing1223@yahoo.com.hk>
2025-04-10 18:59:13 +0800

    Add files via upload

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+
+import '../styles/globals.css'
+import { useRouter } from 'next/router'
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  return <Component {...pageProps} key={router.asPath} />
+}
+
+export default MyApp
