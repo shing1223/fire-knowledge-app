1531fdcca2a9eebba094e3e3f5cca5be6ff7e7b6
shing1223 <shing1223@yahoo.com.hk>
2025-04-11 17:33:21 +0800

    Update layout.tsx

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lato, Noto_Sans_TC } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
