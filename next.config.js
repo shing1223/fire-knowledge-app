819ae79a9af5d1283fef7570dfb39eacd6544ea2
shing1223 <shing1223@yahoo.com.hk>
2025-04-11 19:49:46 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
-}
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true
+})
+
+module.exports = withPWA({
+  reactStrictMode: true
+})
---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="zh-Hant">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/icon-192.png" />
+        <meta name="theme-color" content="#ff5722" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
