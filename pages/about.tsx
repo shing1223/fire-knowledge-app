5801d371181686c9a21f10f42f37e10bee71abd0
shing1223 <shing1223@yahoo.com.hk>
2025-04-12 00:33:35 +0800

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -29,7 +29,7 @@ const AboutPage = () => {
 
       {/* Footer */}
             <footer className="bg-white shadow-inner border-t text-sm py-4 w-full">
-        <div className="grid grid-cols-3 gap-2 w-full text-center px-4 border border-dashed border-blue-500">
+        <div>
           <Link href="/quiz" legacyBehavior>
             <a className="bg-[var(--primary-light)] hover:bg-[var(--primary-dark)] text-white py-3 font-semibold block rounded border border-red-400">📘 問答</a>
           </Link>
