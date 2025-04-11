1a31ea9fb4d8af486b0d23e991ed6d05c01401b8
shing1223 <shing1223@yahoo.com.hk>
2025-04-11 17:58:51 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,11 @@
 import "../styles/globals.css" // ✅ 全域 CSS 放在這裡
+import { Noto_Sans_TC } from "next/font/google"
+
+const noto = Noto_Sans_TC({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-sans"
+})
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
