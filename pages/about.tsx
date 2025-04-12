0cb9538beb6c130681b1dcc74dae61aaf8d38a12
shing1223 <shing1223@yahoo.com.hk>
2025-04-12 17:31:33 +0800

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,7 @@ const AboutPage = () => {
     <div className="flex flex-col justify-between min-h-screen">
       {/* Header */}
       <header className="py-6 text-center bg-white shadow">
-        <h1 className="text-4xl font-extrabold text-red-600">👤 關2於我</h1>
+        <h1 className="text-4xl font-extrabold text-red-600">👤 關23於我</h1>
       </header>
 
       {/* Main */}
