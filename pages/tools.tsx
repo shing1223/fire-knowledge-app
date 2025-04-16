4c43e63e2dc00ee2c4c1fed271b11b660bd5d85c
shing1223 <shing1223@yahoo.com.hk>
2025-04-16 11:54:39 +0800

    Update tools.tsx

--- pages/tools.tsx
+++ pages/tools.tsx
@@ -43,8 +43,8 @@ const ToolsPage = () => {
         </header>
 
         {/* Main */}
-        <main className="flex-1 flex items-center justify-center px-4 py-6 pb-32 bg-gray-50 text-center">
-          <div className="max-w-4xl w-full pb-32">
+        <main className="flex-1 flex items-center justify-center px-4 py-6 pb-20 bg-gray-50 text-center">
+          <div className="max-w-4xl w-full pb-20">
             <input
               type="text"
               placeholder="請輸入工具名稱（中或英文）..."
