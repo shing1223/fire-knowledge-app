f26b6676c723b049bfe50a2ab4c0a77d07c9f910
shing1223 <shing1223@yahoo.com.hk>
2025-04-21 22:49:43 +0800

    Update tools.tsx

--- pages/tools.tsx
+++ pages/tools.tsx
@@ -39,7 +39,7 @@ const ToolsPage = () => {
       <div className="flex flex-col min-h-screen">
         {/* Header */}
         <header className="bg-[var(--header-bg)] text-white text-center font-bold shadow px-4 py-2 sm:py-3 md:py-4 text-sm sm:text-lg md:text-xl">
-          🧰 消防工具搜尋
+          🧰 工具搜尋
         </header>
 
         {/* Main */}
